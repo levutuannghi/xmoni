eb667f7116ca272b658790b1d2c4070096505371
levutuannghi <levutuannghi@gmail.com>
2026-04-12 09:46:10 +0700

    fix: bank logos use object-fit cover to fill icon (v31)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// XMoni Service Worker - Network-first caching
-const CACHE_NAME = 'xmoni-v30';
+﻿// XMoni Service Worker - Network-first caching
+const CACHE_NAME = 'xmoni-v31';
 const ASSETS = [
     './',
     './index.html',
